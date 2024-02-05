bcaee66bb2a48317fdf748177f874f478a9b7ea8
Jakob Socan <10517617+jakgra@users.noreply.github.com>
2024-02-05 16:48:06 +0100

    Test

--- nek.py
+++ nek.py
@@ -1,13 +1,16 @@
 import json
 
-res = [
-    {
-        "test": "something",
-        "out": "neki"
-    },
-    {
-        "test": "something 2",
-        "out": "neki 2"
-    },
-]
-print(json.dumps(res))
+res = {
+    "include":
+    [
+        {
+            "project": "foo",
+            "config": "Debug"
+        },
+        {
+            "project":"bar",
+            "config": "Release"
+        }
+    ]
+}
+print("matrix=" + json.dumps(res))
